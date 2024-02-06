294a3d114704d6e44d56b8775151c97543435f93
Aiden Taylor <amt26480@gmail.com>
2024-02-05 20:29:05 -0500

    first commit

---
+++ baseResponses.py
@@ -0,0 +1,10 @@
+baseResponses = {
+    "hello": """speak("hello, i am jarvis, a specialized talking robot")""",
+    "10 addict":"""speak("...89610900410661997739225625991821890625")""",
+    "how are you":"""speak("i am doing well, how about you?")""",
+    "i'm good":"""speak("that is great, i hope you enjoy your day")""",
+    "i'm not feeling well":"""speak("that is not good, i hope you feel better soon")""",
+    "i'm not feeling good":"""speak("that is not good, i hope you feel better soon")""",
+    "kys":"""speak("*cries in disbelief*"); import keyboard; keyboard.press('alt'); keyboard.press('f4'); keyboard.release('f4'); keyboard.release('alt')""",
+    "goodbye":"""speak("goodbye friend"); quit()""",
+}
